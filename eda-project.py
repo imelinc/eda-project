1f78a6ad578e7e4e5a04dc2d3b47220451e51196
imelinc <ignaciomelinc@gmail.com>
2025-05-13 21:45:41 -0300

    Made everything more compact by using side_functions

--- eda-project.py
+++ eda-project.py
@@ -5,21 +5,6 @@ import seaborn as sns
 import squarify
 import side_functions as sf
 
-def filling_data(df):
-    """
-    this function fills the missing values in the dataset with "Unknown"
-    Args:
-        df (_type_): dataset
-    """
-    df['director'] = df['director'].fillna('Unknown')
-    df['cast'] = df['cast'].fillna('Unknown')
-    df['country'] = df['country'].fillna('Unknown')
-    df['date_added'] = df['date_added'].fillna('Unknown')
-    df['rating'] = df['rating'].fillna('Unknown')
-    df['duration'] = df['duration'].fillna('Unknown')
-    
-    return df
-
 # Plot the distribution of the different types of genres
 def plot_genre_distribution(df):
     """
@@ -36,8 +21,8 @@ def plot_genre_distribution(df):
     tv_shows_df = df[df['type'] == 'TV Show'].copy()
     
     # Get the number of ocurrences of each genre
-    movies_genre_counts = sf.get_genre_counts(movies_df)
-    tv_shows_genre_counts = sf.get_genre_counts(tv_shows_df)
+    movies_genre_counts = sf.get_counts_of_certain_column(movies_df, "listed_in")
+    tv_shows_genre_counts = sf.get_counts_of_certain_column(tv_shows_df, "listed_in")
     
     # Plotting the data
     fig, (ax1, ax2) = plt.subplots(nrows = 1,
@@ -46,26 +31,10 @@ def plot_genre_distribution(df):
                                    facecolor = "#000000")
     
     # Plotting for movies
-    movies_genre_counts.plot(kind="bar", ax=ax1, color="#ad1d3c")
-    ax1.set_facecolor("#15130d")
-    ax1.set_title("Distribution of Genres in Movies", color = "white")
-    ax1.set_xlabel("Genres", color = "white")
-    ax1.set_ylabel("Number of Movies", color = "white")
-    ax1.tick_params(colors = "white")
-    for spine in ax1.spines.values():
-        spine.set_color("white")
-    ax1.grid(True, axis='y', color='white', alpha=0.4)
+    sf.make_bar_plot(movies_genre_counts,"Distribution of Genres in Movies", "Genres", "Number of Movies", ax1, "bar")
     
     # Plotting for tv shows
-    tv_shows_genre_counts.plot(kind="bar", ax=ax2, color="#ad1d3c")
-    ax2.set_facecolor("#15130d")
-    ax2.set_title("Distribution of Genres in TV Shows", color = "white")
-    ax2.set_xlabel("Genres", color = "white")
-    ax2.set_ylabel("Number of TV Shows", color = "white")
-    ax2.tick_params(colors = "white")
-    for spine in ax2.spines.values():
-        spine.set_color("white")
-    ax2.grid(True, axis='y', color='white', alpha=0.4)
+    sf.make_bar_plot(tv_shows_genre_counts, "Distribution of Genres in TV Shows", "Genres", "Number of TV Shows", ax2, "bar")
 
     plt.tight_layout()
     plt.subplots_adjust(wspace=0.2)
@@ -97,28 +66,10 @@ def plot_distribution_of_years(df):
     fig, (ax1, ax2) = plt.subplots(2,1, figsize=(8,10), facecolor = "#000000")
     
     # Plot for movies
-    num_movies.plot(kind="barh", ax=ax1, color="#ad1d3c")
-    ax1.set_facecolor("#15130d")
-    ax1.invert_yaxis()
-    ax1.set_ylabel("Year", color="white")
-    ax1.set_xlabel("Number of Additions", color="white")
-    ax1.set_title("Number of Movies Added on Certain Years", color="white")
-    ax1.tick_params(colors = "white")
-    for spine in ax1.spines.values():
-        spine.set_color("white")
-    ax1.grid(True, axis='x', color='white', alpha=0.4)
+    sf.make_bar_plot(num_movies ,"Number of Movies Added on Certain Years", "Number of Additions", "Year", ax1, "barh" )
     
     # Plot for tv shows
-    num_tv_shows.plot(kind="barh", ax=ax2, color="#ad1d3c")
-    ax2.set_facecolor("#15130d")
-    ax2.invert_yaxis()
-    ax2.set_ylabel("Year", color = "white")
-    ax2.set_xlabel("Number of Additions", color = "white")
-    ax2.set_title("Number of TV Shows Added on Certain Years", color = "white")
-    ax2.tick_params(colors = "white")
-    for spine in ax2.spines.values():
-        spine.set_color("white")
-    ax2.grid(True, axis='x', color='white', alpha=0.4)
+    sf.make_bar_plot(num_tv_shows, "Number of TV Shows Added on Certain Years", "Number of Additions", "Year", ax2, "barh")
     
     plt.tight_layout()
     plt.subplots_adjust(hspace=0.3)
@@ -137,23 +88,11 @@ def plot_countries_vs_genres(df):
     tv_shows_df = df[df["type"] == "TV Show"].copy()
     
     # Getting every country separated (MOVIES)
-    movies_df.loc[:, "country"] = movies_df["country"].str.split(",")
-    movies_exploded = movies_df.explode("country")
-    movies_exploded["country"] = movies_exploded["country"].str.strip()
-    
-    # Getting the top ten countries with the most occurrences for movies
-    movies_top_ten = movies_exploded["country"].value_counts()
-    # Delete the "Unknown" from the top ten
+    movies_top_ten = sf.get_counts_of_certain_column(movies_df, "country")
     movies_top_ten = movies_top_ten[movies_top_ten.index != "Unknown"].head(10)
     
     # Getting every country separated (TV SHOWS)
-    tv_shows_df.loc[:, "country"] = tv_shows_df["country"].str.split(",")
-    tv_shows_exploded = tv_shows_df.explode("country")
-    tv_shows_exploded["country"] = tv_shows_exploded["country"].str.strip()
-    
-    # Getting the top ten countries with the most occurrences for tv shows
-    tv_shows_top_ten = tv_shows_exploded["country"].value_counts()
-    # Delete the "Unknown" from the top ten
+    tv_shows_top_ten = sf.get_counts_of_certain_column(tv_shows_df, "country")
     tv_shows_top_ten = tv_shows_top_ten[tv_shows_top_ten.index != "Unknown"].head(10)
     
     sf.make_pie(movies_top_ten, "Top 10 Countries by Movies", "Countries", "pie_chart_top_ten_countries_(MOVIES)")
@@ -180,7 +119,7 @@ def domination_of_genres(df, movies_top_ten, tv_shows_top_ten):
 df = pd.read_csv("data/netflix_titles.csv")
 
 # Fill the null data and store it in the same variable
-df = filling_data(df)
+df = sf.fill_data(df)
 
 plot_genre_distribution(df)
 plot_distribution_of_years(df)
--- side_functions.py
+++ side_functions.py
@@ -1,6 +1,29 @@
 import matplotlib.pyplot as plt
 
+def fill_data(df):
+    """
+    this function fills the missing values in the dataset with "Unknown"
+    Args:
+        df (_type_): dataset
+    """
+    df['director'] = df['director'].fillna('Unknown')
+    df['cast'] = df['cast'].fillna('Unknown')
+    df['country'] = df['country'].fillna('Unknown')
+    df['date_added'] = df['date_added'].fillna('Unknown')
+    df['rating'] = df['rating'].fillna('Unknown')
+    df['duration'] = df['duration'].fillna('Unknown')
+    
+    return df
+
 def make_pie(top_ten, title, legend_title, file_name):
+    """_summary_
+
+       Args:
+           top_ten (_type_): _description_
+           title (_type_): _description_
+           legend_title (_type_): _description_
+           file_name (_type_): _description_
+    """
     # colors to be used in the pie chart
     colors = ["#7F1A1A", "#A35E2D", "#625B7F", "#3F7F4A", "#4E4E4E", 
                "#947F4E", "#6C4A7F", "#377F79", "#9C3131", "#5A5A5A"]
@@ -22,13 +45,28 @@ def make_pie(top_ten, title, legend_title, file_name):
     plt.savefig(f"figs/{file_name}.png", dpi = 800)
 
 
-def get_genre_counts(df):
+def get_counts_of_certain_column(df, column):
     # Divide the "listed_in" column into multiple rows with each genre
-    df.loc[:, "listed_in"] = df["listed_in"].str.split(",")
-    df_exploded = df.explode("listed_in")
-    df_exploded["listed_in"] = df_exploded["listed_in"].str.strip() 
+    df.loc[:, column] = df[column].str.split(",")
+    df_exploded = df.explode(column)
+    df_exploded[column] = df_exploded[column].str.strip() 
     
     # Count the number of ocurrences of each genre
-    df_genre_counts = df_exploded['listed_in'].value_counts()
+    df_genre_counts = df_exploded[column].value_counts()
     
     return df_genre_counts
+
+def make_bar_plot(file, plot_title, xlabel, ylabel, ax, kind):
+    file.plot(kind=kind , ax=ax, color="#ad1d3c")
+    if kind == "barh":
+       ax.invert_yaxis()
+       ax.grid(True, axis='x', color='white', alpha=0.4)
+    else:
+       ax.grid(True, axis='y', color='white', alpha=0.4)
+    ax.set_facecolor("#15130d")
+    ax.set_title(plot_title, color = "white")
+    ax.set_xlabel(xlabel, color = "white")
+    ax.set_ylabel(ylabel, color = "white")
+    ax.tick_params(colors = "white")
+    for spine in ax.spines.values():
+        spine.set_color("white")
